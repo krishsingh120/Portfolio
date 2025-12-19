ea196cb8211d00a6fdd00e02b0afb6507b45c609
krishsingh10386@gmail.com <bt23eci047@iiit.ac.in>
2025-12-19 13:24:59 +0530

    Fix: Resume Url

--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -262,7 +262,7 @@ const profile = {
 
                 {/* Contact Button */}
                 <a
-                  href="https://drive.google.com/file/d/1ck0d_DQfur6GF4dcSizrZ73P9XWAmRLr/view?usp=drive_link"
+                  href="https://drive.google.com/file/d/1uO8pEZmseLyCOTL9YsLB4F5bo-5TAcEW/view?usp=drive_link"
                   className="group relative inline-flex items-center justify-center gap-3 p-0.5 rounded-xl bg-gradient-to-r from-gray-800 to-gray-700 transition-all duration-300 hover:scale-105 hover:shadow-[0_0_2rem_-0.5rem_#60A5FA]"
                 >
                   <span className="block w-full px-6 sm:px-8 py-3 sm:py-4 rounded-[11px] bg-gray-900 border border-gray-700/50 transition-all duration-300 group-hover:bg-gradient-to-r group-hover:from-gray-800 group-hover:to-gray-700">
